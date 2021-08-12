2ecd6515a33b6df67688dad66c779a51588ffd79
UrielJuarez <ujjuarezr@gmail.com>
2021-08-12 12:08:38 -0500

    prueba

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,8 @@ import { connect } from 'mongoose';
 
 export async function startConnectionDB(){
     await connect('mongodb+srv://Gorichelas2021:M3YU0rfFskSgMQZp@cluster0.n6ic6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    //await connect('mongodb:localhost/goris_db',{ 
+    //await connect('mongodb:localhost/goris_db',{
+        
         useNewUrlParser: true,
         useFindAndModify: false
     })
